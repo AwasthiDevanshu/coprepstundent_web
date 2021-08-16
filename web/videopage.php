437ad318c3e4a2a89723c613888d2032fdfa4467
devanshu <devanshu@coprepedu.com>
2021-08-16 22:59:32 +0530

    ch

--- web/videopage.php
+++ web/videopage.php
@@ -8,6 +8,9 @@ require_once("Constant.php");
 
 <head>
     <title>Web Dashboard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="assets/css/dashboard.css">
     <link id="theme-style" rel="stylesheet" href="assets/css/portal.css">
     <link rel="stylesheet" type="text/css" href="assets/css/loader.css">
