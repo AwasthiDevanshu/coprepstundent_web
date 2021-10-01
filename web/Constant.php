1ca0cc3424c7fdeebf604f81e4054cff1395b723
Ajay <coc78360@gmail.com>
2021-10-01 20:19:31 +0530

    vikalp`

--- web/Constant.php
+++ web/Constant.php
@@ -2,10 +2,10 @@
 class Constant
 {
     const BASE_URL = "https://backend.coprepedu.com/";
-    const COMPANYID = 27;
-    const ANDROID_APP_LINK = "https://play.google.com/store/apps/details?id=com.targetwithalok.app";
+    const COMPANYID = 31;
+    const ANDROID_APP_LINK = "#";
     const SUPPORT_EMAIL = "support@coprepedu.com";
-    const COMPANYNAME = "Target With Alok";
+    const COMPANYNAME = "Vikalp Test Series Institute";
     const DEFAULT_PAGE = "index.php";
     const LOGO_URL = "assets/images/Logo New.png";
     const PAGE_MAP =  [
