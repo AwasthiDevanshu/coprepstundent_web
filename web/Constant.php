48674b970f2711b4ae99bc914ef98b8b3159940b
Ajay <coc78360@gmail.com>
2021-10-08 20:14:03 +0530

    param fix

--- web/Constant.php
+++ web/Constant.php
@@ -6,12 +6,12 @@ class Constant
     const ANDROID_APP_LINK = "#";
     const SUPPORT_EMAIL = "support@coprepedu.com";
     const COMPANYNAME = "Vikalp Test Series Institute";
-    const DEFAULT_PAGE = "index.php";
+    const DEFAULT_PAGE = "test.php";
     const LOGO_URL = "assets/images/Logo New.png";
     const PAGE_MAP =  [
-                        "index" => true,
-                        "videopage" => true,
-                        "videolist" => true,
+                        "index" => false,
+                        "videopage" => false,
+                        "videolist" => false,
                         "reset_pass" => true,
                         "test" => true,
                         "current_affairs" => true,
