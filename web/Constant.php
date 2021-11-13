f49b5021cf899134666c3caef97145da684cc935
Ajay <coc78360@gmail.com>
2021-11-13 20:32:33 +0530

    init

--- web/Constant.php
+++ web/Constant.php
@@ -2,10 +2,10 @@
 class Constant
 {
     const BASE_URL = "https://backend.coprepedu.com/";
-    const COMPANYID = 9;
-    const ANDROID_APP_LINK = "https://play.google.com/store/apps/details?id=com.targetwithalok.app";
-    const SUPPORT_EMAIL = "support@coprepedu.com";
-    const COMPANYNAME = "Climax Academy";
+    const COMPANYID = 8;
+    const ANDROID_APP_LINK = "https://play.google.com/store/apps/details?id=com.theselectionaddaapp.app";
+    const SUPPORT_EMAIL = "support@selectionadda.com";
+    const COMPANYNAME = "Selection Adda";
     const DEFAULT_PAGE = "test.php";
     const LOGO_URL = "assets/images/Logo New.png";
     const PAGE_MAP =  [
