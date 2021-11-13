9467a57bce28883f6e4b333a37ad189bbe543a7d
Ajay <coc78360@gmail.com>
2021-11-13 20:38:09 +0530

    url fix

--- web/Constant.php
+++ web/Constant.php
@@ -3,7 +3,7 @@ class Constant
 {
     const BASE_URL = "https://backend.coprepedu.com/";
     const COMPANYID = 32;
-    const ANDROID_APP_LINK = "#";
+    const ANDROID_APP_LINK = "https://play.google.com/store/apps/details?id=com.policeanddefenceacademy.app";
     const SUPPORT_EMAIL = "shrutisanjeev74@gmail.com";
     const COMPANYNAME = "PDA";
     const DEFAULT_PAGE = "test.php";
