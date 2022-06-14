7dcfa1a4c9de427e0d13e418fc75fad9ceaa5e9b
Ajay <coc78360@gmail.com>
2022-06-14 21:04:16 +0530

    premier app

--- Vikalp/web/Constant.php
+++ Vikalp/web/Constant.php
@@ -2,14 +2,14 @@
 class Constant
 {
     const BASE_URL = "https://backend.coprepedu.com/";
-    const COMPANYID = 31;
-    const ANDROID_APP_LINK = "#";
-    const SUPPORT_EMAIL = "support@coprepedu.com";
-    const COMPANYNAME = "Vikalp Test Series Institute";
+    const COMPANYID = 26;
+    const ANDROID_APP_LINK = "https://play.google.com/store/apps/details?id=com.premierlive.app";
+    const SUPPORT_EMAIL = "support@premierlearning01@gmail.com";
+    const COMPANYNAME = "Harendra Learning App";
     const DEFAULT_PAGE = "index.php";
-    const LOGO_URL = "assets/images/Logo New.png";
+    const LOGO_URL = "assets/images/premier-logo.png";
     const PAGE_MAP =  [
-                        "index" => false,
+                        "index" => true,
                         "videopage" => true,
                         "videolist" => true,
                         "reset_pass" => true,
